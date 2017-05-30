eaba5102c148a57b566c84a38d942313df7c2bc4
Isaac Cantun <isaac@grupoicarus.com.mx>
2017-05-30 16:30:39 -0500

    prueba rutas

--- config.php
+++ config.php
@@ -2,5 +2,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 define('APP_ROOT', __DIR__);
 
+//$loader = new Twig_Loader_Filesystem(APP_ROOT . '/views');
 $loader = new Twig_Loader_Filesystem(APP_ROOT . '/public/views');
 $twig = new Twig_Environment($loader, []);
